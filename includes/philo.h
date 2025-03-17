e95577e15648d6139818571163ed5620928901e2
Renan Corlett <renancorlett@gmail.com>
2025-03-17 11:15:36 +0000

    Add files via upload

--- includes/philo.h
+++ includes/philo.h
@@ -23,6 +23,10 @@
 # include <unistd.h>
 # include <stdbool.h>
 
+# define RESET  "\033[0m"
+# define RED    "\033[1;31m"
+# define GREEN  "\033[1;32m"
+
 //---------- MACROS ----------
 
 enum e_forks
