7cf69ed5d4305188ae9ef17cf210ecf10775e005
Renan Corlett <renancorlett@gmail.com>
2025-08-05 12:33:18 +0100

    Update activities.c

--- src/activities.c
+++ src/activities.c
@@ -14,11 +14,11 @@
 
 void	sleeping(t_philo *philo)
 {
-	if (!is_dead(philo) && philo->status != SLEEPING)
+	if (!is_dead(philo) && philo->status != SLEEPING) //Verifies if he is dead or if he has the status of sleeping)
 	{
-		philo->status = SLEEPING;
-		logs(philo, philo->status);
-		wait_time(philo, philo->data->t_sleep);
+		philo->status = SLEEPING; //Set the status to Sleeping
+		logs(philo, philo->status); //Print the status
+		wait_time(philo, philo->data->t_sleep); //Wait for the time configure to sleep
 	}
 }
 
@@ -28,25 +28,25 @@ void	thinking(t_philo *philo)
 	{
 		philo->status = THINKING;
 		logs(philo, philo->status);
-		wait_time(philo, 5);
+		wait_time(philo, 5); //Fix time to think, avoiding the philo thinks too much and starves
 	}
 }
 
 static bool	get_fork(t_philo *philo, int fork)
 {
-	pthread_mutex_lock(philo->fork[fork]);
-	if (is_dead(philo))
+	pthread_mutex_lock(philo->fork[fork]); //Tries to block the choosen fork
+	if (is_dead(philo)) //if the philo dies in the process, unlock the mutex
 	{
 		pthread_mutex_unlock(philo->fork[fork]);
 		return (false);
 	}
-	logs(philo, FORK);
+	logs(philo, FORK); //If not, store the info about the chosen fork
 	return (true);
 }
 
-static bool	get_forks(t_philo *philo)
+static bool	get_forks(t_philo *philo) //Tries to take the left fork, tries to take the right fork, if fails, free the left fork to the  other, if gets both, it is true
 {
-	if (!get_fork(philo, LEFT))
+	if (!get_fork(philo, LEFT)) 
 		return (false);
 	if (!get_fork(philo, RIGHT))
 	{
@@ -58,15 +58,15 @@ static bool	get_forks(t_philo *philo)
 
 void	eating(t_philo *philo)
 {
-	if (!get_forks(philo))
+	if (!get_forks(philo)) //Tries to take the forks, if fails return ERROR
 		return ;
-	pthread_mutex_lock(&philo->lock);
+	pthread_mutex_lock(&philo->lock); //Block the philo to update the time limit of death
 	philo->t_death = get_time() + philo->data->t_death;
 	pthread_mutex_unlock(&philo->lock);
-	philo->status = EATING;
+	philo->status = EATING; //Set the status and stored in log
 	logs(philo, philo->status);
-	philo->meals++;
-	wait_time(philo, philo->data->t_eat);
-	pthread_mutex_unlock(philo->fork[LEFT]);
-	pthread_mutex_unlock(philo->fork[RIGHT]);
+	philo->meals++; //Increment the number of meals
+	wait_time(philo, philo->data->t_eat); //Waits the time to eat
+	pthread_mutex_unlock(philo->fork[LEFT]); //Free the fork
+	pthread_mutex_unlock(philo->fork[RIGHT]); //Free the fork
 }
