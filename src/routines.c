900e14590ac51575602a39ff82f7234d95c48d53
Renan Corlett <renancorlett@gmail.com>
2025-08-05 11:41:36 +0100

    Update routines.c

--- src/routines.c
+++ src/routines.c
@@ -14,34 +14,34 @@
 
 void	logs(t_philo *p, int status)
 {
-	pthread_mutex_lock(&p->data->log);
-	if (status == DEATH)
+	pthread_mutex_lock(&p->data->log); //Block the lock for each thread write one by one
+	if (status == DEATH) //If death, prints the time of start and identifies the philo dead
 		printf("%lu %lu died\n", get_time() - p->data->t_start, p->id);
-	else if (status == EATING)
+	else if (status == EATING) //Print the time, ID, and say that is eating
 		printf("%lu %lu is eating\n", get_time() - p->data->t_start, p->id);
-	else if (status == SLEEPING)
+	else if (status == SLEEPING) //Print the time, ID, and say that is sleeping
 		printf("%lu %lu is sleeping\n", get_time() - p->data->t_start, p->id);
-	else if (status == THINKING)
+	else if (status == THINKING) //Print the time, ID, and say that is thinking
 		printf("%lu %lu is thinking\n", get_time() - p->data->t_start, p->id);
-	else if (status == FORK)
+	else if (status == FORK) //Print the time, ID, and say that take the fork
 		printf("%lu %lu has taken a fork\n",
 			get_time() - p->data->t_start, p->id);
-	pthread_mutex_unlock(&p->data->log);
+	pthread_mutex_unlock(&p->data->log); //Unlock to other thread writes in the log
 }
 
 void	*routine(void *philo_ptr)
 {
 	t_philo	*philo;
 
-	philo = (t_philo *) philo_ptr;
-	pthread_mutex_lock(&philo->lock);
-	philo->t_death = philo->data->t_start + philo->data->t_death;
-	pthread_mutex_unlock(&philo->lock);
-	if (philo->id % 2 == 0 || philo->id == philo->data->n_philos)
+	philo = (t_philo *) philo_ptr; // Convert the point to identify as philo
+	pthread_mutex_lock(&philo->lock); //Block the mutex of the philo to protect his data
+	philo->t_death = philo->data->t_start + philo->data->t_death; //Define the limit of time for death if the philo does not eat
+	pthread_mutex_unlock(&philo->lock); //Unlock the mutex of the philo
+	if (philo->id % 2 == 0 || philo->id == philo->data->n_philos) //If the philo has the ID pair or is the last start thinking
 		thinking(philo);
-	if (is_solo(philo))
-		return (NULL);
-	while (!is_dead(philo))
+	if (is_solo(philo)) //Verifies is is just one philo, he only takes one fork and finishes the thread because he dies
+		return (NULL); //Return ERROR
+	while (!is_dead(philo)) //While the phino is not dead, his status is changing in that one
 	{
 		eating(philo);
 		sleeping(philo);
